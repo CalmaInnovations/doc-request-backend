6ee7029ecee314783964590003e6b5185d50660e
Kurt Reyes Oré <142367162+KurtRO26@users.noreply.github.com>
2025-01-03 16:02:35 +0000

    Update url

--- src/main/java/com/calma/DocManagerServer/services/serviceImpl/DocumentoServiceImpl.java
+++ src/main/java/com/calma/DocManagerServer/services/serviceImpl/DocumentoServiceImpl.java
@@ -42,7 +42,7 @@ public class DocumentoServiceImpl implements DocumentoService {
     private final JavaMailSender mailSender;
     private static final String UPLOAD_DIR = "src/main/resources/files/Prueba1.xlsx";
 
-    private static final String BASE_URL = "http://localhost:8080/api/downloadPdf";
+    private static final String BASE_URL = "https://improved-xylophone-pjrw9vj7j74q3vjw-8080.app.github.dev/api/downloadPdf";
     private static final String BUTTON_STYLE = "background-color: #00BFFF; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 4px; border: none;";
 
     @Override
